84a7f0062809d7c1da82305b4353d030d759c253
Tenderfeel <tenderfeel@gmail.com>
2020-02-04 02:45:06 +0900

    algolia test
    add searchform

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,73 @@
-<template lang="pug">
-  nav.navbar.is-dark.is-fixed-top.level.is-mobile(role="navigation" aria-label="main navigation")
-    h1.navbar-brand
-      span.navbar-item 100BM
-    .level.is-mobile.navbar-item
-      .level-item.has-text-centered(v-if="data")
-        div.has-text-white(:class="{'has-text-success': isFinish}")
-          span Completed: {{ completeTotal }}/{{data.length}}
-          span(v-if="isFinish" style="margin-left: 1rem") Finish!!
+<template>
+  <nav
+    class="navbar is-dark is-fixed-top level is-mobile"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <h1 class="navbar-brand">
+      <span class="navbar-item">100BM</span>
+    </h1>
+    <div class="navbar-item">
+      <ais-instant-search :search-client="searchClient" index-name="characters">
+        <ais-autocomplete>
+          <div slot-scope="{ currentRefinement, indices, refine }">
+            <input
+              type="search"
+              :value="currentRefinement"
+              placeholder="Input name..."
+              autocomplete="off"
+              @input="refine($event.currentTarget.value)"
+            >
+            <div v-if="currentRefinement">
+              <b-navbar-dropdown
+                arrowless
+                hoverable
+                active
+                aria-role="list"
+                class="is-mobile"
+                v-for="index in indices"
+                :key="index.label"
+              >
+                <b-navbar-item
+                  aria-role="listitem"
+                  v-for="hit in index.hits"
+                  :key="hit.name"
+                >{{hit.name}}</b-navbar-item>
+              </b-navbar-dropdown>
+            </div>
+          </div>
+        </ais-autocomplete>
+      </ais-instant-search>
+    </div>
+    <div class="level is-mobile navbar-item">
+      <div class="level-item has-text-centered" v-if="data">
+        <div class="has-text-white" :class="{'has-text-success': isFinish}">
+          <span>
+            Completed: {{ completeTotal }}/{{data.length}}
+            <span
+              v-if="isFinish"
+              style="margin-left: 1rem;"
+            >Finish!!</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 <script>
-import { mapState, mapGetters } from "vuex";
+import algoliasearch from "algoliasearch/lite";
+import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      searchResults: [],
+      selected: null,
+      searchClient: algoliasearch(
+        "KFZDNJLM1A",
+        "da33181b4c2cd6cc8dd868273b4359d3"
+      )
+    };
+  },
   computed: {
     ...mapState(["data"]),
     completeTotal() {
@@ -23,3 +80,9 @@ export default {
 };
 </script>
 
+<style scoped>
+input[type="search"] {
+  --webkit-ime-mode: inactive;
+  ime-mode: inactive;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ import Field from "buefy/dist/components/field";
 import Switch from "buefy/dist/components/switch";
 import Button from "buefy/dist/components/button";
 import Toast from "buefy/dist/components/toast";
-
+import Navbar from "buefy/dist/components/navbar";
 import "buefy/dist/buefy.css";
+import InstantSearch from "vue-instantsearch";
 
 Vue.config.productionTip = false;
-
+Vue.use(Navbar);
 Vue.use(Table);
 Vue.use(Field);
 Vue.use(Switch);
 Vue.use(Button);
 Vue.use(Toast);
+Vue.use(InstantSearch);
 
 new Vue({
   store,
